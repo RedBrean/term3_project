0db5b5c5d96f76daf26033358c7f34aa39c10891
vlad <u@u.u>
2024-12-21 15:21:10 +0300

    fix

--- include/out_client.h
+++ include/out_client.h
@@ -5,9 +5,40 @@
 
 
 
-class Out_Client
-{
+#include <boost/asio.hpp>
+#include <string>
 
+using boost::asio::ip::tcp;
 
-    
+class Out_Client {
+private:
+    std::string login_;       // Логин клиента
+    bool is_connected_;       // Подключён ли клиент
+    tcp::socket socket_;      // Сокет клиента
+
+public:
+    // Конструктор принимает готовый сокет и логин клиента
+    Out_Client(tcp::socket&& socket, const std::string& login)
+        : login_(login), is_connected_(true), socket_(std::move(socket)) {}
+
+    // Геттеры
+    const std::string& get_login() const {
+        return login_;
+    }
+
+    bool is_connected() const {
+        return is_connected_;
+    }
+
+    tcp::socket& get_socket() {
+        return socket_;
+    }
+
+    // Метод для отключения клиента
+    void disconnect() {
+        if (is_connected_) {
+            socket_.close();
+            is_connected_ = false;
+        }
+    }
 };
